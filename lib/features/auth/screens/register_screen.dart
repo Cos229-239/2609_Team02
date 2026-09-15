898201c53d2dd5d6be9c947236e11ef30fa43e5f
babyrhedd <ashleynicole0100@icloud.com>
2026-09-15 19:59:14 -0400

    Update account creation fields

--- lib/features/auth/screens/register_screen.dart
+++ lib/features/auth/screens/register_screen.dart
@@ -79,7 +79,11 @@ class _RegisterScreenState extends State<RegisterScreen> {
               children: [
                 AuthTextField(
                   controller: _nameController,
+<<<<<<< HEAD
                   label: 'Full Name',
+=======
+                  label: 'First and Last Name *',
+>>>>>>> 233958e (Update account creation fields)
                   icon: Icons.badge_outlined,
                   validator: (v) => Validators.required(v, fieldName: 'Name'),
                 ),
@@ -113,7 +117,11 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 const SizedBox(height: 12),
                 AuthTextField(
                   controller: _confirmPasswordController,
+<<<<<<< HEAD
                   label: 'Confirm Password',
+=======
+                  label: 'Confirm Your Password *',
+>>>>>>> 233958e (Update account creation fields)
                   icon: Icons.lock_outline,
                   obscureText: true,
                   textInputAction: TextInputAction.done,
